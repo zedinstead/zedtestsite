3d8b9061ef4eb503b9af5d711d2c322fc292baa5
zedinstead <89502941+zedinstead@users.noreply.github.com>
2022-03-19 17:10:57 -0700

    nineteenth

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -27,8 +27,8 @@ const Home = () => {
                 <div className="contain1">
                     <div class="col-lg-5">
                     <h1 class="font-weight-dark"></h1>
-                        <p>the power of art </p>
-                        <p>can change the world</p>
+                        <p>zedinstead: the website </p>
+                        
                     </div>
                 </div>
             </center>
